74d6fccce9a3699a6c3f00cace0507a68e96039c
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-11-04 13:16:55 +0300

    edit contact form

--- imports/startup/helpers.js
+++ imports/startup/helpers.js
@@ -3,3 +3,11 @@ const moment = require('moment');
 Template.registerHelper('formatDate', (date) => {
   return moment(date).format('DD MMMM YYYY');
 });
+
+Template.registerHelper('formatInputDate', (date) => {
+  return moment(date).format('YYYY-MM-DD');
+});
+
+Template.registerHelper('selectedIfEquals', (value, check) => {
+  return value == check ? 'selected' : '';
+})
