ba63a044cc41e231f3c538fb723a82c241e2b55c
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-11-05 17:52:52 +0300

    search view

--- imports/ui/contacts/search.js
+++ imports/ui/contacts/search.js
@@ -5,5 +5,9 @@ import { ContactsIndex } from '../../api/contacts';
 Template.searchContacts.helpers({
   'contactsIndex' () {
     return ContactsIndex;
+  },
+
+  'inputAttributes'() {
+    return { 'class': 'form-control', 'id': 'searchTerm'}
   }
 });
