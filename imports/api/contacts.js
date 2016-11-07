a8a1a2ec33bdee03516764fbd1b3f31612d9238b
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-11-07 19:40:20 +0300

    Add Schema

--- imports/api/contacts.js
+++ imports/api/contacts.js
@@ -1,5 +1,72 @@
 export const Contacts = new Mongo.Collection('contacts');
 
+let Schemas = {};
+
+Schemas.Phones = new SimpleSchema({
+
+  type: {
+    type: String,
+    allowedValues: ['personal', 'work']
+  },
+
+  phone: {
+    type: String
+  }
+});
+
+Schemas.Emails = new SimpleSchema({
+
+  type: {
+    type: String,
+    allowedValues: ['personal', 'work']
+  },
+
+  email: {
+    type: String
+  }
+});
+
+Schemas.Contacts = new SimpleSchema({
+  firstName: {
+    type: String
+  },
+
+  lastName: {
+    type: String
+  },
+
+  contactType: {
+    type: String,
+    allowedValues: ["friends", "colleagues"],
+    optional: true
+  },
+
+  birthday: {
+    type: Date,
+    optional: true
+  },
+
+  phones: {
+    type: [Schemas.Phones],
+    optional: true
+  },
+
+  emails: {
+    type: [Schemas.Emails],
+    optional: true
+  },
+
+  createdAt: {
+    type: Date
+  },
+
+  owner: {
+    type: String
+  }
+});
+
+Contacts.attachSchema(Schemas.Contacts);
+
 export const ContactsIndex = new EasySearch.Index({
   collection: Contacts,
   fields: ['firstName'],
